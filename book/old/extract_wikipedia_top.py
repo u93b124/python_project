26640ba5ecd8d1e3b48ed64feed8f119451859b6
u93b124 <u93b124@gmail.com>
2022-07-18 10:19:47 +0000

    bookディレクトリ削除

--- book/old/extract_wikipedia_top.py
+++
@@ -1,6 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-page_data = requests.get('https://ja.wikipedia.org/wiki/Python').text
-page = BeautifulSoup(page_data, 'lxml')
-for element in page.select("#siteSub"):print(element.txt)
